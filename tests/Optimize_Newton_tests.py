07200e0b0475cf7b8997dcfb125fc42889025527
Hanna Autio <tna11hau@hacke-12.student.lth.se>
2014-09-26 13:37:18 +0200

    Created tests for the _solveEquations method in the newton optimizer class.

--- tests/Optimize_Newton_tests.py
+++ tests/Optimize_Newton_tests.py
@@ -48,11 +48,26 @@ class TestNewtonCall:
 		self.testFunction.evalgrad(np.array([1,2]))
 
 
+class TestSolveEquations:
 
+    def setUp(self):
+        self.optimizer=OptimizeNewton.OptimizeNewton()
+        self.b=np.array([1., 1., 1.])
 
+    def tearDown(self):
+        del self.optimizer
 
+    @raises(ValueError)
+    def testNotPositiveDefinite(self):
+        A=np.array([[1.,2.,5.],[0.,-3.,6],[0.,0.,4]])
+        c=self.optimizer._solveEquations(A,b)
 
+    def testReturnsArray(self):
+        A=np.eye(3)
+        c=self.optimizer._solveEquations(A,b)
+        assert issinstance(c, np.array)
 
-
-
-
+    def testCorrectAnswer(self):
+        A=np.array([[1.,2.,5.],[0.,3.,6.],[0.,0.,4.]])
+        c=self.optimizer._solveEquations(A,b)
+        assert np.allclose(c,np.array([1./12., -1./6., 1./4.]))
