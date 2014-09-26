126d9ea4ed6c2a7960d0b6cc89222272c01cd921
Hanna Autio <tna11hau@hacke-12.student.lth.se>
2014-09-26 13:39:56 +0200

    added self. where necessary

--- tests/Optimize_Newton_tests.py
+++ tests/Optimize_Newton_tests.py
@@ -60,14 +60,14 @@ class TestSolveEquations:
     @raises(ValueError)
     def testNotPositiveDefinite(self):
         A=np.array([[1.,2.,5.],[0.,-3.,6],[0.,0.,4]])
-        c=self.optimizer._solveEquations(A,b)
+        c=self.optimizer._solveEquations(A,self.b)
 
     def testReturnsArray(self):
         A=np.eye(3)
-        c=self.optimizer._solveEquations(A,b)
+        c=self.optimizer._solveEquations(A,self.b)
         assert issinstance(c, np.array)
 
     def testCorrectAnswer(self):
         A=np.array([[1.,2.,5.],[0.,3.,6.],[0.,0.,4.]])
-        c=self.optimizer._solveEquations(A,b)
+        c=self.optimizer._solveEquations(A,self.b)
         assert np.allclose(c,np.array([1./12., -1./6., 1./4.]))
