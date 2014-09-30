2357832146a96fbae81b52e6cb2232fd517c9d08
Erik Jonsson-Glans <erik.jg@gmail.com>
2014-09-30 11:17:30 +0200

    I changed some imports and corrected syntax errors.

--- tests/Optimize_Generic_tests.py
+++ tests/Optimize_Generic_tests.py
@@ -2,18 +2,17 @@ import sys
 sys.path = sys.path + ['../']
 from nose.tools import raises,with_setup
 import numpy as np
-import Function
-import Newton
+import Optimize
 
 class TestNewtonInit:
 
     @raises(TypeError)
     def testTolNotFloat(self):
-        self.testFunction = Newton.Newton(1,1)
+        self.testFunction = Optimize.OptimizeNewton(1,1)
 
     @raises(TypeError)
     def testMaxIterationsNotInt(self):
-        self.testFunction = Newton.Newton(1.,1.2)
+        self.testFunction = Optimize.OptimizeNewton(1.,1.2)
 	
 
 class TestNewtonCall:
@@ -21,14 +20,14 @@ class TestNewtonCall:
     def setUp(self):
 	def f(self,x,y):
 	    return (x**2)+(y**2)
-        self.testFunction = Function.Function(f)
-	self.testOptimize = Newton.Newton()
+        self.testFunction = Optimize.Function(f)
+	self.testOptimize = Optimize.OptimizeNewton()
 
     def tearDown(self):
         del self.testFunction
 	del self.testOptimize
 
-    @raise(TypeError)
+    @raises(TypeError)
     def testReturnArray(self):
 	val = self.testOptimize(self.testFunction,np.array([1.,1.]))
 	assert isinstance(val,np.ndarray)
@@ -62,8 +61,8 @@ class TestNewtonStep:
     def setUp(self):
 	def f(self,x,y):
 	    return (x**2)+(y**2)
-        self.testFunction = Function.Function(f)
-	self.testOptimize = Newton.Newton()
+        self.testFunction = Optimize.Function(f)
+	self.testOptimize = Optimize.OptimizeNewton()
 
     def tearDown(self):
         del self.testFunction
